fc13154dfe3f4a1fe297329fb03d1c0f30016c95
hohuuphuong <hohuuphuong2804@gmail.com>
2023-12-09 17:22:47 +0700

    create repository BookRepository

---
+++ src/main/java/com/group7/bookshopwebsite/repository/BookRepository.java
@@ -0,0 +1,34 @@
+package com.group7.bookshopwebsite.repository;
+
+import com.group7.bookshopwebsite.entity.Book;
+import com.group7.bookshopwebsite.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+	Page<Book> findByTitleContaining(String keyword, Pageable pageable);
+
+	Book findByTitle(String title);
+
+	Page<Book> findByCategory(Category category, Pageable pageable);
+
+	Page<Book> findByCategory_IdAndTitleContaining(Long categoryId, String keyword, Pageable pageable);
+
+	Page<Book> findByCategoryIdAndTitleContainingOrderBySalePriceDesc(Long categoryId, String keyword,
+			Pageable pageable);
+
+	Page<Book> findByCategoryIdAndTitleContainingOrderBySalePriceAsc(Long categoryId, String keyword,
+			Pageable pageable);
+
+	Page<Book> findByCategoryIdAndTitleContainingOrderByCreatedAtAsc(Long categoryId, String keyword,
+			Pageable pageable);
+
+	Page<Book> findByCategoryIdAndTitleContainingOrderByCreatedAtDesc(Long categoryId, String keyword,
+			Pageable pageable);
+
+	
+
+
+}
